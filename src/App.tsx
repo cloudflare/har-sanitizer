97da30d4ecbe1deb3f1fef34bc5d12c171ceae21
James Royal <jroyal@cloudflare.com>
2023-10-25 11:07:34 -0500

    Add subheader and move scrub initial state out of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App() {
 		<div className="flex flex-col justify-center place-items-start min-h-[100dvh] mx-auto max-w-7xl p-4 md:p-6 lg:p-8">
 			<div className="w-full space-y-6">
 				<h1 className="text-3xl font-bold">Sanitize your HAR file</h1>
+				<p className="text-sm">
+					Protect your session data by sanitizing your HAR files before sharing.
+					Built with Cloudflare Workers.
+				</p>
 				<UploadHar setUpload={setUpload} />
 				{!!upload && (
 					<Sanitizer input={upload.raw} name={upload.name}></Sanitizer>
